db4fb8bc9798f9e01343bc0c7a37bd72e9595506
TAKU1101 <taku.kuma1101@icloud.com>
2021-02-27 22:26:18 +0900

    add: func

--- discordbot.py
+++ discordbot.py
@@ -18,4 +18,18 @@ async def ping(ctx):
     await ctx.send('pong!!!')
 
 
+@bot.command()
+async def minesweeper(ctx):
+    await.send('''||:zero:||||:zero:||||:zero:||||:one:||||:one:||||:two:||||:two:||||:two:||||:one:||||:zero:||
+||:zero:||||:one:||||:two:||||:three:||||:bomb:||||:two:||||:bomb:||||:bomb:||||:one:||||:zero:||
+||:one:||||:two:||||:bomb:||||:bomb:||:three::two::two:||:two:||||:one:||||:zero:||
+||:one:||||:bomb:||||:four:||||:bomb:||:two::zero::zero:||:zero:||||:zero:||||:zero:||
+||:one:||||:one:||||:three:||||:two:||:two::zero::zero:||:zero:||||:zero:||||:zero:||
+||:one:||||:one:||||:one:||||:bomb:||||:one:||||:zero:||||:zero:||||:zero:||||:zero:||||:zero:||
+||:bomb:||||:one:||||:one:||||:two:||||:two:||||:one:||||:zero:||||:zero:||||:zero:||||:zero:||
+||:one:||||:one:||||:zero:||||:one:||||:bomb:||||:one:||||:zero:||||:zero:||||:zero:||||:zero:||
+||:zero:||||:zero:||||:zero:||||:one:||||:one:||||:one:||||:zero:||||:zero:||||:zero:||||:zero:||
+||:zero:||||:zero:||||:zero:||||:zero:||||:zero:||||:zero:||||:zero:||||:zero:||||:zero:||||:zero:||''')
+
+
 bot.run(token)
